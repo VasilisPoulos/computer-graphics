27940aca822538e4d7ea356238142caa11079bfe
Stelios Zappidis <Stelioszapp@gmail.com>
2020-11-27 17:26:05 +0200

    Contains the working source files
    No duplication in this (I hope)

---
+++ Last_Update/res/shaders/shader.glsl
@@ -0,0 +1,46 @@
+#shader fragment
+#version 330 core
+                                                                       
+// Interpolated values from the vertex shaders                         
+in vec2 UV;                                                            
+in vec3 color;
+                                                          
+// Ouput data                                                          
+out vec4 out_color;                                                        
+                                                                       
+// Values that stay constant for the whole mesh.                       
+uniform sampler2D myTextureSampler;                                    
+uniform vec4 textureFlag;
+uniform float transparency;
+
+void main() {                                                          
+                                                                       
+    // Output color = color of the texture at the specified UV  
+    // using textureFlag to set a color if there is there is no texture
+    out_color = textureFlag * vec4((texture(myTextureSampler, UV).rgb), 1.0f) + (1.0 - textureFlag) * vec4( color, transparency);                         
+}
+
+#shader vertex
+#version 330 core 
+// Input vertex data, different for all executions of this shader.     
+layout(location = 0) in vec3 vertexPosition_modelspace;                
+layout(location = 1) in vec2 vertexUV;                                 
+                                                                       
+// Output data ; will be interpolated for each fragment.               
+out vec2 UV;                                                           
+out vec3 color;              
+
+// Values that stay constant for the whole mesh.                       
+uniform mat4 MVP;                                                      
+uniform vec3 set_color;                                                                       
+
+void main() {                                                          
+                                                                       
+    // Output position of the vertex, in clip space : MVP * position   
+    gl_Position = MVP * vec4(vertexPosition_modelspace, 1);            
+                                                                 
+    // UV of the vertex. No special space for this one.                
+    UV = vertexUV;
+
+    color = set_color;
+}
---
+++ Last_Update/src/ShaderProgram.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <GL/glew.h>
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+
+struct ShaderProgramSource
+{
+	std::string VertexShaderSource;
+	std::string FragmentShaderSource;
+};
+
+class ShaderProgram
+{
+public:
+	ShaderProgram();
+	~ShaderProgram();
+
+	void bind()	const { glUseProgram(m_ProgramID); }
+	void unbind() const { glUseProgram(0); }
+
+	void setUniform4f(const std::string& uniform, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
+	void setUniform4fv(const std::string& uniform, GLsizei count, GLboolean transpose, const GLfloat* value);
+	GLint getAttribLocation(const GLchar* attribute) { return glGetAttribLocation(m_ProgramID, attribute);}
+	GLint getUniformLocation(const std::string& uniform);
+private:
+	const char* shaderPath = "res/shaders/shader.glsl";
+	GLuint m_ProgramID;
+	std::unordered_map<std::string, GLint> m_UniformLocationCache;
+
+	ShaderProgramSource parseShaders(const std::string& filepath);
+	GLuint createShaders(const std::string& vertexShader, const std::string& fragmentShader);
+	GLuint compileShader(GLuint shaderType, const std::string& shaderSource);
+};
